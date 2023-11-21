b18abcb1018c661e94575f6989befbc24ceffc50
MrBisquit <william@wtdawson.info>
2023-11-21 18:58:12 +0000

    Add project files.

---
+++ ParticleSim/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using Ookii.Dialogs.Wpf;
+using ParticleSim.Types;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ParticleSim
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap MainBM = new Bitmap(1600, 900); // 16:9 ratio
+        Bitmap PBM = new Bitmap(4, 4); // 4x4 because yes
+
+        List<Particle> Particles = new List<Particle>();
+
+        int CurrentTick = 0; // Keeps on going up
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            TaskDialog td = new TaskDialog()
+            {
+                WindowTitle = "ParticleSim",
+                Content = "Press Run to start the simulation! And click it again to pause/stop it.\nBy default hovering/clicking on a particle will enlarge it on the right.\n" +
+                "To spawn particles:\n" +
+                "- At the top, click Particles\n" +
+                "- Click Spawn Particles\n" +
+                "- Click on the particle type you want"
+            };
+
+            TaskDialogButton tdb = new TaskDialogButton()
+            {
+                Text = "Close the main window to close this",
+                Enabled = false, // L
+            };
+
+            td.Buttons.Add(tdb);
+
+            Task.Factory.StartNew(() => { td.Show(); }); // New thread so it doesen't freeze the current one
+
+            SpawnParticles(300, ParticleType.Gas); // Spawn some basic
+
+            // Clearing the Bitmaps
+            // Not the most efficient way but it does what it needs to
+            for (int i = 0; i < MainBM.Width; i++)
+            {
+                for (int j = 0; j < MainBM.Height; j++)
+                {
+                    MainBM.SetPixel(i, j, System.Drawing.Color.Black);
+                }
+            }
+
+            for (int i = 0; i < PBM.Width; i++)
+            {
+                for (int j = 0; j < PBM.Height; j++)
+                {
+                    PBM.SetPixel(i, j, System.Drawing.Color.Black);
+                }
+            }
+
+            UpdateImages(); // Update the images
+        }
+
+        private void UpdateImages()
+        {
+            Main.Source = ConvertToBitmapImage(MainBM);  // Set the big image
+            Particle.Source = ConvertToBitmapImage(PBM); // Set the smaller image next to the big image
+
+            ParticlesCount.Content = $"Particles: {Particles.Count}";
+        }
+
+        // Bing AI, I know how it works (I don't)
+        public BitmapImage ConvertToBitmapImage(Bitmap bitmap)
+        {
+            using (var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                return bitmapImage;
+            }
+        }
+
+        private void SpawnParticles(int Amount, ParticleType ParticleType, bool KeepExisting = false)
+        {
+            if(!KeepExisting) Particles.Clear();
+
+            for (int i = 0; i < Amount; i++)
+            {
+                Particles.Add(new Particle(0, 0, ParticleType, 0, 0, 0, 0));
+            }
+
+            Tick();
+            UpdateImages();
+        }
+
+        private void Tick()
+        {
+            CurrentTick++;
+
+            Particles.ForEach(Particle =>
+            {
+                MainBM = Particle.Tick(CurrentTick % 10, MainBM); // RIP your computer
+            });
+        }
+    }
+}
---
+++ ParticleSim/Types/Particle.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParticleSim.Types
+{
+    internal class Particle
+    {
+        public int temperature { get; set; } = 0; // In degrees centigrade
+        public int speed { get; set; } = 0; // Depending on the temp
+
+        /* Rates of speed:
+         * Freezing = 0 movements per 10 ticks (Unless Gas)
+         * Cold = 0-5 movements per 10 ticks
+         * Normal = 5-10 movements per 10 ticks
+         * Warm = 10-15 movements per 10 ticks
+         */
+
+        public ParticleType type { get; set; } = ParticleType.Gas; // Gas as just the default, this wont matter as it'll be set when the class is initialised.
+
+        // It's location
+        public int x { get; set; } = 0;
+        public int y { get; set; } = 0;
+
+        // Specific particle location
+        public int px { get; set; } = 0;
+        public int py { get; set; } = 0;
+
+        public Particle(int temperature, int speed, ParticleType type, int x, int y, int px, int py)
+        {
+            this.temperature = temperature;
+            this.speed = speed;
+            this.type = type;
+            this.x = x;
+            this.y = y;
+            this.px = px;
+            this.py = py;
+        }
+
+        public Bitmap Tick(int tick, Bitmap BM)
+        {
+            Color color = Color.White;
+
+            if(type == ParticleType.Gas)
+            {
+                color = Color.Orange; // Because why not
+                color = Color.FromArgb(255 / (temperature + 1), color.R, color.G, color.B); // Trust me, I have no clue what I'm doing either
+            }
+
+            BM.SetPixel(x, y, color);
+
+            return BM;
+        }
+    }
+
+    internal enum ParticleType
+    {
+        Gas = 0,       // Moves freely
+        Liquid = 1,    // Is a puddle, because water
+        Solid = 2,     // Is block
+        Ice_cream = 3, // Felt like it
+        Plasma = -1    // No
+    }
+}
